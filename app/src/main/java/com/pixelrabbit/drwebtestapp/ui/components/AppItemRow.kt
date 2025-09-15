5fb8f5d6215cfdcb2f61f68115401c9f0ec00da2
FrolovIlia <ilia_frolov@list.ru>
2025-09-15 13:29:33 +0300

    Оптимизировал код

--- app/src/main/java/com/pixelrabbit/drwebtestapp/ui/components/AppItemRow.kt
+++ app/src/main/java/com/pixelrabbit/drwebtestapp/ui/components/AppItemRow.kt
@@ -1,6 +1,5 @@
 package com.pixelrabbit.drwebtestapp.ui.components
 
-
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
